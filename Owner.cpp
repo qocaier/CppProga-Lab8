3e2e1becd94a710f739e267eb0e3a84c86ab4957
qocaier <kh.maxim2017@gmail.com>
2025-05-13 15:44:58 +0300

    ÐOwner create

---
+++ Owner.cpp
@@ -0,0 +1,16 @@
+#include "Owner.h"
+
+void Owner::addProperty(Property* property)
+{
+
+}
+
+void Owner::delProperty(unsigned int n)
+{
+
+}
+
+double Owner::calcSumTax()
+{
+	return 0.0;
+}
---
+++ Owner.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+#include <vector>
+#include "Property.h"
+
+class Owner
+{
+	string fullname;
+	string inn;
+	vector<Property*> properties;
+public:
+	Owner(string fn, string nn) : fullname(fn), inn(nn) {};
+
+	void addProperty(Property* property);
+	void delProperty(unsigned int n);
+	double calcSumTax();
+};
